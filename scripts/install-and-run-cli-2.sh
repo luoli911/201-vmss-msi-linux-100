ecf1c0b7b099d70ab7b3bdc399160dc90033febc
Li Luo (Wicresoft North America) <v-liiluo@microsoft.com>
2018-03-29 17:22:16 +0800

    modify stressmcr.sh to mount on /mnt/azurefiles again

--- scripts/install-and-run-cli-2.sh
+++ scripts/install-and-run-cli-2.sh
@@ -48,3 +48,22 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 sudo apt-get -y update
 sudo apt-get -y install docker-ce
 
+sudo apt-get -y update
+sudo apt-get install cifs-utils
+
+
+today=$(date +%Y-%m-%d)
+sudo mkdir /mnt/azurefiles
+sudo mount -t cifs //acrtestlogs.file.core.windows.net/logshare /mnt/azurefiles -o vers=3.0,username=acrtestlogs,password=ZIisPCN0UrjLfhv6Njiz0Q8w9YizeQgIm6+DIfMtjak4RJrRlzJFn4EcwDUhNvXmmDv5Axw9yGePh3vn1ak8cg==,dir_mode=0777,file_mode=0777,sec=ntlmssp
+sudo mkdir /mnt/azurefiles/$today
+
+echo "---docker pull dotnet from mcr.microsoft.com---"
+pullbegin=$(date +%s%3N)
+PullStartTime=$(date +%H:%M:%S)
+sudo docker pull mcr.microsoft.com/dotnet
+pullend=$(date +%s%3N)
+PullEndTime=$(date +%H:%M:%S)
+pulltime=$((pullend-pullbegin))
+echo "---nslookup mcr.microsoft.com---"
+nslookup=$(nslookup mcr.microsoft.com)
+echo registry,region,starttime,endtime,pulltime:mcr,eastus,$PullStartTime,$PullEndTime,$pulltime >> /mnt/azurefiles/$today/mcr-output.log
