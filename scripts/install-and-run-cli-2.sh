d9fd1d6b7819f681e425bdba00f64f68b90a9a8f
Li Luo (Wicresoft North America) <v-liiluo@microsoft.com>
2018-03-29 14:48:56 +0800

    stress test with template deployed

---
+++ scripts/install-and-run-cli-2.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+while getopts ":i:a:c:r:" opt; do
+  case $opt in
+    i) docker_image="$OPTARG"
+    ;;
+    a) storage_account="$OPTARG"
+    ;;
+    c) container_name="$OPTARG"
+    ;;
+    r) resource_group="$OPTARG"
+    ;;
+    p) port="$OPTARG"
+    ;;
+    t) script_file="$OPTARG"
+    ;;
+    \?) echo "Invalid option -$OPTARG" >&2
+    ;;
+  esac
+done
+
+if [ -z $docker_image ]; then
+    docker_image="azuresdk/azure-cli-python:latest"
+fi
+
+if [ -z $script_file ]; then
+    script_file="writeblob.sh"
+fi
+
+for var in storage_account resource_group
+do
+
+    if [ -z ${!var} ]; then
+        echo "Argument $var is not set" >&2
+        exit 1
+    fi 
+
+done
+
+# Install Docker and then run docker image with cli
+
+sudo apt-get -y update
+sudo apt-get -y install linux-image-extra-$(uname -r) linux-image-extra-virtual
+sudo apt-get -y update
+sudo apt-get -y install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get -y update
+sudo apt-get -y install docker-ce
+
+today=$(date +%Y-%m-%d)
+sudo mkdir ~/logshare
+sudo mount -t cifs //acrtestlogs.file.core.windows.net/logshare logshare -o vers=3.0,username=acrtestlogs,password=ZIisPCN0UrjLfhv6Njiz0Q8w9YizeQgIm6+DIfMtjak4RJrRlzJFn4EcwDUhNvXmmDv5Axw9yGePh3vn1ak8cg==,dir_mode=0777,file_mode=0777,sec=ntlmssp
+sudo mkdir ~/logshare/$today
+
+echo "---docker pull dotnet from mcr.microsoft.com---"
+pullbegin=$(date +%s%3N)
+PullStartTime=$(date +%H:%M:%S)
+sudo docker pull mcr.microsoft.com/dotnet
+pullend=$(date +%s%3N)
+PullEndTime=$(date +%H:%M:%S)
+pulltime=$((pullend-pullbegin))
+echo "---nslookup mcr.microsoft.com---"
+nslookup=$(nslookup mcr.microsoft.com)
+echo registry,region,starttime,endtime,pulltime:mcr,eastus,$PullStartTime,$PullEndTime,$pulltime >> ~/logshare/$today/mcr-output.log
