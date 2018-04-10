42322c6006cbe424799a900b10d8785095197746
HarveyLink <30464227+HarveyLink@users.noreply.github.com>
2018-04-10 10:54:23 +0800

    Update install-and-run-cli-2.sh

--- scripts/install-and-run-cli-2.sh
+++ scripts/install-and-run-cli-2.sh
@@ -39,15 +39,6 @@ do
 
 done
 
-# Install Azure CLI and then build image with az
-
-sudo apt-get -y update
-sudo apt-get -y install linux-image-extra-$(uname -r) linux-image-extra-virtual
-sudo apt-get -y update
-sudo apt-get -y install apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get -y update
 
 #Install Azure CLI
 AZ_REPO=$(lsb_release -cs)
@@ -75,7 +66,6 @@ function loadTest()
 sudo mkdir /mnt/azurefiles/$today
 sudo mkdir /mnt/azurefiles/$today/Scenario1
 sudo mkdir /mnt/azurefiles/$today/Scenario1/$machineName$currenttime
-
 
 ACR_NAME="NewACRLoadTestBuildCR"
 sudo git clone https://github.com/SteveLasker/node-helloworld.git
