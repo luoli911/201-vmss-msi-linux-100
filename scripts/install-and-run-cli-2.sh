d0a9b6e3e68d0b74c5a21153e85d2a2d92bd041b
luoli911 <v-liiluo@microsoft.com>
2018-04-12 11:04:12 +0800

    Update install-and-run-cli-2.sh

--- scripts/install-and-run-cli-2.sh
+++ scripts/install-and-run-cli-2.sh
@@ -80,33 +80,33 @@ function loadTest()
 ACR_NAME="NewACRLoadTestBuildCR100"
 sudo git clone https://github.com/SteveLasker/node-helloworld.git
 cd node-helloworld
-echo "az login -u azcrci@microsoft.com -p $password"
+echo "+ az login -u azcrci@microsoft.com -p $password"
 az login -u azcrci@microsoft.com -p $password
 
-echo "az account set --subscription c451bd61-44a6-4b44-890c-ef4c903b7b12"
+echo "+ az account set --subscription c451bd61-44a6-4b44-890c-ef4c903b7b12"
 az account set --subscription "c451bd61-44a6-4b44-890c-ef4c903b7b12"
 
-echo "az extension remove -n acrbuildext"
+echo "+ az extension remove -n acrbuildext"
 az extension remove -n acrbuildext
 
-echo  "az extension add --source https://acrbuild.blob.core.windows.net/cli/acrbuildext-0.0.2-py2.py3-none-any.whl -y"
+echo  "+ az extension add --source https://acrbuild.blob.core.windows.net/cli/acrbuildext-0.0.2-py2.py3-none-any.whl -y"
 az extension add --source https://acrbuild.blob.core.windows.net/cli/acrbuildext-0.0.2-py2.py3-none-any.whl -y
 
-echo "az acr login -n $ACR_NAME"
+echo "+ az acr login -n $ACR_NAME"
 az acr login -n $ACR_NAME
 
 echo "---ACR Build Test---"
 pullbegin=$(date +%s%3N)
 PullStartTime=$(date +%H:%M:%S)
-#for i in {1..100} 
-#  do    
-#   echo "az acr build -t helloworld1:v1 --context . -r $ACR_NAME"
-#   az acr build -t helloworld$i:v1 --context . -r $ACR_NAME 
-#   echo "BuildTask$i Done!" 
-#  done
-echo "az acr build -t helloworld1:v1 --context . -r $ACR_NAME"
-az acr build -t helloworld1:v1 --context . -r $ACR_NAME 
-echo "BuildTask1 Done!" 
+for i in {1..2} 
+  do    
+   echo "+ az acr build -t helloworld1:v1 --context . -r $ACR_NAME"
+   az acr build -t helloworld$i:v1 --context . -r $ACR_NAME 
+   echo "BuildTask$i Done!" 
+  done
+# echo "+ az acr build -t helloworld1:v1 --context . -r $ACR_NAME"
+# az acr build -t helloworld1:v1 --context . -r $ACR_NAME 
+# echo "BuildTask1 Done!" 
 pullend=$(date +%s%3N)
 PullEndTime=$(date +%H:%M:%S)
 pulltime=$((pullend-pullbegin))
